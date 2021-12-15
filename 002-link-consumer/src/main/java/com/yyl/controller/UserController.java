e6aaaaa4477d882f152e55136932548b0beb76ed
yyl <3136720835@qq.com>
2021-12-15 11:33:24 +0800

    直连方式简单示例

--- 002-link-consumer/src/main/java/com/yyl/controller/UserController.java
+++ 002-link-consumer/src/main/java/com/yyl/controller/UserController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("user")
 @AllArgsConstructor
-public class UserController implements BeanPostProcessor {
+public class UserController {
     private UserService userService;
 
 
